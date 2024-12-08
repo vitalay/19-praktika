8ec111b2a015ecef85063704fde0547f69ae978c
vitalya.efimov <vitalay.efimov@gmail.com>
2024-12-08 21:38:24 +0500

    настроил кнопку выход

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -3,30 +3,31 @@
     <h3>Online Bank</h3>
     <ul class="navbar-menu">
       <li><router-link to="/">Заявки</router-link></li>
-       <li><router-link to="/">Помощь</router-link></li>
+      <li><router-link to="/">Помощь</router-link></li>
       <li><a href="#">Сообщение</a></li>
-      <li><a href="#">выход</a></li>
-
+      <li><a href="#" @click.prevent="logout">выход</a></li>
     </ul>
-
-     
-   
-
   </nav>
 </template>
 
 <script>
-
-import { useRoute } from 'vue-router';
-import { useStore } from 'vuex';
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
 
 export default {
-
   setup() {
-    const router = useRoute();
+    const router = useRouter();
     const store = useStore();
-  }
-   
+
+    return {
+
+      logout:() => {
+        store.commit("auth/logout");
+        router.push("/auth");
+        
+      }
+    };
+  },
 };
 </script>
 
